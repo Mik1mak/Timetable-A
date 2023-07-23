178c19a9c05f0470b53e6ccfa81aa4861ceb12f0
Mikołaj Makowski <308125@o365.stud.umk.pl>
2023-07-23 12:31:57 +0200

    Update RavenDB AutoMapperProfile

--- TimetableA.DataAccessLayer.RavenDB/AutoMapperProfile.cs
+++ TimetableA.DataAccessLayer.RavenDB/AutoMapperProfile.cs
@@ -12,8 +12,20 @@ public class AutoMapperProfile : Profile
 {
     public AutoMapperProfile()
     {
-        CreateMap<Lesson, Lesson>();
-        CreateMap<Group, Group>();
-        CreateMap<Timetable, Timetable>();
+        CreateMap<Lesson, Lesson>()
+            .ForMember(l => l.GroupId, opt => opt.Ignore())
+            .ForMember(l => l.Group, opt => opt.Ignore())
+            .ForMember(l => l.Id, opt => opt.Ignore());
+        CreateMap<Group, Group>()
+            .ForMember(g => g.Id, opt => opt.Ignore())
+            .ForMember(g => g.TimetableId, opt => opt.Ignore())
+            .ForMember(g => g.Timetable, opt => opt.Ignore())
+            .ForMember(g => g.Lessons, opt => opt.Ignore());
+        CreateMap<Timetable, Timetable>()
+            .ForMember(t => t.Id, opt => opt.Ignore())
+            .ForMember(t => t.ReadKey, opt => opt.Ignore())
+            .ForMember(t => t.EditKey, opt => opt.Ignore())
+            .ForMember(t => t.CreateDate, opt => opt.Ignore())
+            .ForMember(t => t.Groups, opt => opt.Ignore());
     }
 }
